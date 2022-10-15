e2a28fa4456f01860585147dc79ef44b748e5782
umanizer <arata1005@yahoo.co.jp>
2022-10-15 17:05:02 +0900

    create reducer for events

---
+++ src/reducers/index.js
@@ -0,0 +1,27 @@
+// action = {
+//   type: "CREATE_EVENT",
+//   title:"東京オリンピックのお知らせ",
+//   body:"東京でオリンピックを開催します。つきましては....."
+// };
+
+const events = (state = [], action) => {
+  switch (action.type) {
+    case "CREATE_EVENT":
+      return [
+        ...state,
+        {
+          id: state.length + 1,
+          title: action.title,
+          body: action.body,
+        },
+      ];
+    case "DELETE_EVENT":
+      return state;
+    case "DELETE_ALL_EVENTS":
+      return [];
+    default:
+      return state;
+  }
+};
+
+export default events;
